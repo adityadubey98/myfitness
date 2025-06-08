a2aa2268ad942224af97779cfd700ca966a8d2b8
Aditya Dubey <dubeyadi9661@gmail.com>
2025-06-08 12:40:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ import UserRoutes from "./routes/User.js"
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://fitnesstrackeradi.netlify.app'
-  ],
-  credentials: true // if you use cookies/auth
-}));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
